3cb569d883d9c8c28b8459b536c31fdf232c6370
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-09-12 15:09:20 +0900

    a

--- src/lib/assert.ts
+++ src/lib/assert.ts
@@ -1,4 +1,4 @@
-export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+export const assertIsDefined = <T>(val: T): asserts val is NonNullable<T> => {
   if (val === undefined || val === null) {
     throw new Error(`Expected 'val' to be defined, but received ${val}`)
   }
